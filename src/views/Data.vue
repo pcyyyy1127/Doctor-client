40e7ced066d27f1fdeb7e81b263f0a89e62eba64
gzk <867847040@qq.com>
2022-01-23 15:16:45 +0800

    导出随访数据功能

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -39,7 +39,20 @@
             },
             exportFollow(){
 
+                this.$axios.get("/doctor/follow/export",{responseType: "blob"}).then((res) => {
 
+                    let link = document.createElement("a");
+                    //    后端返回的是文件流 用Blob来接收二进制文件流 并制定类型和编码
+                    let blob = new Blob([res.data],{type:"application/vnd.ms-excel;charset=utf-8"})
+                    let objectUrl = window.URL.createObjectURL(blob);
+                    link.href = objectUrl;
+                    let date = new Date();
+                    let name = "随访历史数据-" + date.getFullYear()+ "年" + date.getMonth()+ 1 + "月" + date.getDate() + "日"
+                        +date.getHours()+ "时" + date.getMinutes() + "分" + date.getSeconds() + "秒" + ".xls";
+                    link.download = name;
+                    link.click();
+                    window.URL.revokeObjectURL(objectUrl);
+                });
 
             },
         }
